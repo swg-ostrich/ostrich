a74c467aef6d2c3381302f663164441e057669d2
seefo <robbie.uvanni@gmail.com>
2019-04-09 06:49:56 -0400

    Fixed a few issues with debian initial setup

--- utils/initial-setup/debian/setup.sh
+++ utils/initial-setup/debian/setup.sh
@@ -17,34 +17,46 @@ sudo apt-get update
 # install base dependencies
 if [[ $arch == "i386" ]]; then
 	echo "Setting up 32 bit build env..."
-	sudo apt-get install build-essential zlib1g-dev libpcre3-dev cmake psmisc \
+	sudo apt-get install build-essential zlib1g-dev libpcre3-dev psmisc \
 		libboost-dev libxml2-dev libncurses5-dev flex bison git-core alien libaio1 \
 		python-ply bc libcurl4-gnutls-dev clang-3.9 ant curl -y
 else
 	echo "Setting up 64 bit build env..."
 	sudo apt-get install lib32z1 lib32ncurses5 g++-6-multilib gcc-6-multilib clang-3.9 zlib1g-dev:i386 libc6:i386 psmisc clang \
 		libc6-dev:i386 libc6-i686:i386 libgcc1:i386 linux-libc-dev:i386 \
-		zlib1g:i386 libpcre3-dev:i386 cmake libxml2-dev:i386 libncurses5-dev:i386 \
+		zlib1g:i386 libpcre3-dev:i386 libxml2-dev:i386 libncurses5-dev:i386 \
 		flex bison git-core alien libaio1:i386 python-ply bc libaio1 \
 		libboost-dev build-essential libc6-dbg:i386 libc6-dbg libcurl4-gnutls-dev:i386 ant curl -y
 
 	sudo apt-get remove libxml2-dev:amd64 libncurses-dev:amd64 zlib1g-dev:amd64
 fi
 
+# install cxmake from src to fix CXX17 problems
+cd /tmp
+wget http://www.cmake.org/files/v3.8/cmake-3.8.2.tar.gz && \
+     tar xf cmake-3.8.2.tar.gz && \
+     cd cmake-3.8.2            && \
+     ./configure               && \
+     make && sudo make install
+
 # install git lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install git-lfs -y
 
+# prepare to install external dependencies
 cd $basedir/dependencies
+cp * /tmp
+cd /tmp
 
 # install java
 tar -xvzf jdk-8u152-linux-i586.tar.gz
 sudo mv jdk1.8.0_152/ /opt
 sudo ln -s /opt/jdk1.8.0_152 /usr/java
 
-# nuke old versions
+# nuke old versions of oracle instant client
 sudo rm -rf /usr/lib/oracle &> /dev/null
 
+# install oracle instant client
 if [ $arch == "i386" ]; then
 	sudo alien -i oracle-instantclient12.2-basiclite-12.2.0.1.0-1.i386.rpm
 	sudo alien -i oracle-instantclient12.2-devel-12.2.0.1.0-1.i386.rpm
@@ -64,11 +76,11 @@ sudo touch /etc/ld.so.conf.d/oracle.conf
 export ORACLE_HOME="/usr/lib/oracle/12.2/client"
 export JAVA_HOME=/usr/java
 
-cd $basedir;
+cd $basedir
 
 # Set java include paths - you want to change these to something like the below for oracle
-sudo cp utils/init/java_ldsoconfd_example.conf /etc/ld.so.conf.d/java.conf
-sudo cp utils/init/java_profile_example.sh /etc/profile.d/java.sh
+sudo cp utils/initial-setup/debian/java_ldsoconfd_example.conf /etc/ld.so.conf.d/java.conf
+sudo cp utils/initial-setup/debian/java_profile_example.sh /etc/profile.d/java.sh
 
 echo "/usr/lib/oracle/12.2/client/lib" | sudo tee -a /etc/ld.so.conf.d/oracle.conf
 
