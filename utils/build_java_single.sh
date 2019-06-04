80e5dee689d9c34f8245f0f2cd9e4a735f48eb7e
seefo <robbie.uvanni@gmail.com>
2019-06-04 18:36:48 -0400

    Removed very old, deprecated build scripts

--- utils/build_java_single.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-destination="data/sku.0/sys.server/compiled/game"
-sourcepath="dsrc/sku.0/sys.server/compiled/game"
-
-mkdir -p $destination/script
-
-javac -Xlint:-options -encoding utf8 -classpath "$destination" -d "$destination" -sourcepath "$sourcepath" -g -deprecation "$1"
--- utils/build_tdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-basedir=$PWD
-
-find $basedir/dsrc -name '*.tdf' | while read filename; do
-	echo $filename
-	$basedir/exe/linux/bin/TemplateDefinitionCompiler -compile $filename
-done
